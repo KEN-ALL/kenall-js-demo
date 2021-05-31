86e8f207167235ea0a33dfe2094fc314467e3784
Moriyoshi Koizumi <moriyoshi.koizumi@open-c.jp>
2021-05-31 09:02:44 +0900

    Update.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,9 @@ const Index: React.FunctionComponent = () => {
       <h1 className="mb-5 text-3xl font-bold text-gray-500">ケンオール デモページ</h1>
     </header>
     <main>
-      <p className="mb-2">サイドバーからデモを選択してください。</p>
+      <p className="mb-2 hidden md:block">サイドバーからデモを選択してください。</p>
+      <p className="mb-2 md:hidden">右上のハンバーガーメニューからデモを選択してください。</p>
+      <p className="mb-2">このデモのソースコードは<a href="https://github.com/ken-all/kenall-js-demo">こちら</a>です。</p>
     </main>
   </section>
 };
