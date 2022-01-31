055fdb7ae6324529ede4f9086e8e960b0fa5493f
Moriyoshi Koizumi <moriyoshi.koizumi@open-c.jp>
2022-01-31 17:27:13 +0900

    fix: default API endpoint

--- src/kenall.ts
+++ src/kenall.ts
@@ -1,7 +1,7 @@
 import { KENALL } from '@ken-all/kenall';
 
 const apiBaseUrl =
-  process.env.REACT_APP_KENALL_API_BASE_URL || 'https://api-beta.kenall.jp/v1';
+  process.env.REACT_APP_KENALL_API_BASE_URL;
 const api = new KENALL(process.env.REACT_APP_KENALL_API_KEY as string, {
   apibase: apiBaseUrl,
   timeout: 10000,
